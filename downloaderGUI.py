50ddaa1faa027585adc9152dcd7a523e6509528f
JuPa <jupapl4ys@gmail.com>
2023-12-16 11:24:30 +0100

    working gui with rescaling and scrollbar

--- downloaderGUI.py
+++ downloaderGUI.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 from pytube import Playlist, YouTube
 from moviepy.editor import *
-
+from tkinter.scrolledtext import ScrolledText
 class Downloader:
     window = None
     linkListContainer = None
@@ -11,23 +11,49 @@ class Downloader:
     message = None
     progressBar = None
     destroyCount = 0
+    frame_id = None
+    canvas  = None
     def __init__(self):
+        #https://www.youtube.com/playlist?list=PL3Qgpx2KozbJezBzfQ82jLiEeZwjEtejc
         self.window = window = tk.Tk()
         window.title("YouTube Downloader")
         window.geometry("600x400")
+        
 
-        self.linkListContainer = video_frame = ttk.Frame(master = window)
+        container = ttk.Frame(self.window)
+        self.canvas = canvas = tk.Canvas(container)
+        scrollbar = ttk.Scrollbar(container, orient="vertical", command=canvas.yview)
+
+        self.linkListContainer = scrollable_frame = ttk.Frame(canvas)
+        scrollable_frame.pack(fill="x",side="right", expand=True)
+        scrollable_frame.bind(
+            "<Configure>",
+            lambda e: canvas.configure(
+                scrollregion=canvas.bbox("all")
+            )
+        )
+
+        self.frame_id = frameID = canvas.create_window((0, 0), window=scrollable_frame, anchor="nw",width=380)#nw
+        canvas.configure(yscrollcommand=scrollbar.set)
+        container.pack(fill="both",expand=True,side="top", anchor='nw')
+        canvas.pack(side="left", fill="both", expand=True,padx=5)
+        scrollbar.pack(side="right", fill="y")
+        canvas.bind('<MouseWheel>', lambda event: canvas.yview_scroll(-int(event.delta / 60), "units"))
+        canvas.bind("<Configure>", self.resize_frame)
+        scrollable_frame.bind('<MouseWheel>', lambda event: canvas.yview_scroll(-int(event.delta / 60), "units"))
         self.addLinkEntry()
-        video_frame.pack(side='top',expand=False, fill='x', padx=10)
 
+        
+        
         fetch_frame = ttk.Frame(master = window)
-        self.downloadType = tk.StringVar(value="Select Type")
-        w = ttk.OptionMenu(fetch_frame, self.downloadType ,"MP3", "MP4") 
+        self.downloadType = tk.StringVar()
+        options = ["MP3", "MP4"]
+        optionMenu = ttk.OptionMenu(fetch_frame, self.downloadType , "Select Type", *options)
         fetchButton = ttk.Button(master=fetch_frame, text="Download", command=self.fetch)
         fetchButton.pack(side='left')
         destroyButton = ttk.Button(master=fetch_frame, text="Clear All", command=self.destroy)
         destroyButton.pack(side='left')
-        w.pack(side='left')
+        optionMenu.pack(side='left')
         fetch_frame.pack(side='bottom', pady=(0,5))
 
         message_frame = ttk.Frame(master = window, height=40)
@@ -39,7 +65,15 @@ class Downloader:
         self.progressBar.pack_forget()
         self.message.configure(text="Please insert any YouTubeLink into the link text field")
         message_frame.pack(side='bottom',expand=False, pady=(0,5))
+        style = ttk.Style(window)
+        style.configure("success.TLabel", foreground="black", background="green")          
+        style.configure("error.TLabel", foreground="black", background="red")
         window.mainloop()
+
+
+
+    def resize_frame(self, e):
+        self.canvas.itemconfig(self.frame_id, width=(e.width-5))
 
     def addLinkEntry(self, videoLink=None):
         link = tk.StringVar()
@@ -47,57 +81,67 @@ class Downloader:
         append = False
         if (videoLink == None):
             append = True
+
+        link_frame = ttk.Frame(master = self.linkListContainer)
         link.trace_add('write',callback=lambda x, y, z, sv=link: self.callback(link, title, append))
-        linkEntry = ttk.Entry(master=self.linkListContainer, textvariable=link)
-        linkTitle = ttk.Entry(master=self.linkListContainer,textvariable=title)
-        deleteEntry = ttk.Button(master=self.linkListContainer, text="Delete", command= lambda: self.deleteEntry(linkEntry, linkTitle, deleteEntry))
-        deleteEntry.pack(side='right',expand=False,fill='y',pady=(10,0))
-        linkEntry.pack(expand=True, fill='x', pady=(10,0))
+        linkEntry = ttk.Entry(master=link_frame, textvariable=link)
+        linkTitle = ttk.Entry(master=link_frame,textvariable=title)
+        deleteEntry = ttk.Button(master=link_frame, text="Delete", command= lambda: self.deleteEntry(link_frame))
+        deleteEntry.pack(side='right',expand=False,fill='y')
+        linkEntry.pack(expand=True, fill='x')
         linkTitle.pack(expand=True, fill='x')
+        link_frame.pack(pady=(10,0),expand=True, fill='x')
+        linkEntry.bind('<MouseWheel>', lambda event: self.canvas.yview_scroll(-int(event.delta / 60), "units"))
+        linkTitle.bind('<MouseWheel>', lambda event: self.canvas.yview_scroll(-int(event.delta / 60), "units"))
+        deleteEntry.bind('<MouseWheel>', lambda event: self.canvas.yview_scroll(-int(event.delta / 60), "units"))
         if (videoLink != None):
             link.set(videoLink)
-        self.entryList.append([linkEntry,linkTitle, deleteEntry])
+        self.entryList.append(link_frame)
 
-    def deleteEntry(self, linkEntry, linkTitle, deleteEntry):
-        deleteEntry.pack_forget()
-        linkEntry.pack_forget()
-        linkTitle.pack_forget()
-        self.entryList.remove([linkEntry,linkTitle, deleteEntry])
+    def deleteEntry(self, link_frame):
+        link_frame.pack_forget()
+        self.entryList.remove(link_frame)
         self.addLinkEntry()
 
     def fetch(self):
+        self.message.configure(text="Starting download...")
         wrong = False
-        filyType = self.downloadType.get() 
-        if (filyType != "Select Type"):
+        fileType = self.downloadType.get() 
+        if (fileType != "Select Type"):
             lenOfList = len(self.entryList)
-            self.message.configure(text=f"Starting download of {lenOfList-1} items...")
             progress = tk.IntVar()
             self.progressBar.configure(maximum=lenOfList,variable=progress)
             progress.set(0)
             self.progressBar.pack()
             count = 0
             for element in self.entryList:
-                link = element[0].get() # link
-                title = element[1].get()
-                if (self.videoOrPlaylist(link) == 0):
-                    #download video
-                    if (filyType == "MP3"):
-                        self.downloadFile(link, 0, title)
-                    if (filyType == "MP4"):
-                        self.downloadFile(link, 1, title)
-                    style = ttk.Style()
-                    style.configure("BW.TLabel", foreground="black", background="green")
-                    element[0].configure(style="BW.TLabel")
-                    element[1].configure(style="BW.TLabel")
-                else:
-                    wrong = True
-                    style = ttk.Style()
-                    style.configure("BW.TLabel", foreground="black", background="red")
-                    element[0].configure(style="BW.TLabel")
-                    element[1].configure(style="BW.TLabel")
+                children = element.winfo_children()
+                if (len(children)>0):
+                    link = children[0].get() # link
+                    title = children[1].get()
+                    if (len(link)>0 and len(title)>0):
+                        if (self.videoOrPlaylist(link) == 0):
+                            #download video
+                            title = self.cleanTitle(title)
+                            if (fileType == "MP3"):
+                                self.downloadFile(link, 0, title)
+                            elif (fileType == "MP4"):
+                                self.downloadFile(link, 1, title)
+                            children[0].configure(style="success.TLabel")
+                            children[1].configure(style="success.TLabel")
+                           
+                        else:
+                            wrong = True
+                            children[0].configure(style="error.TLabel")
+                            children[1].configure(style="error.TLabel")
+                            
+                    else:
+                        children[0].configure(style="")
+                        children[1].configure(style="")
+                        #link to short
+                        pass
                 count = count+1
                 self.progressBar.step(count)
-            
             if (wrong == False):
                 self.message.configure(text="Download Complete!")
             else:
@@ -106,6 +150,10 @@ class Downloader:
         else:
             self.message.configure(text="Please select a filetype!")
             
+    def cleanTitle(self,title):
+        title = title.replace('\'',"").replace("/","").replace(":","").replace("*","").replace("?","").replace('"',"").replace('"',"").replace('<',"").replace('<',"").replace('|',"")
+        return title
+
 
     def downloadFile(self,link,fileType, title):
         v = YouTube(link)
@@ -120,7 +168,8 @@ class Downloader:
         if (fileType == 1): #mp4
             v.streams.get_highest_resolution().download(output_path=str(outputPath), filename=(title+'.mp4'))
         else:
-            print("Wrong filetype")
+            #wrong filetype
+            pass
 
 
     def videoOrPlaylist(self, str):
@@ -141,6 +190,7 @@ class Downloader:
         elif(self.videoOrPlaylist(link) == 1):
             #fetch all entries
             videos = self.getPlaylistVideos(link)
+            titleBar.set("Fetching Playlist...")
             for video in videos:
                 self.addLinkEntry(video)
         else:
@@ -176,15 +226,10 @@ class Downloader:
             self.destroyCount+=1
         else:
             for entry in self.entryList:
-                entry[0].pack_forget()
-                entry[1].pack_forget()
-                entry[2].pack_forget()
+                entry.pack_forget()
             self.entryList.clear()
             self.addLinkEntry()
-            #window = self.window
-            #window.destroy()
-            #self.__init__()
-        
+            self.message.configure(text="Please insert any YouTubeLink into the link text field")      
     
 def main():
     d = Downloader()
